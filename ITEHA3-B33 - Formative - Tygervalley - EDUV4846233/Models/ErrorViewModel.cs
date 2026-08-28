d11bc210414ac7be192de8db641b0ac5ffd343c7
Nicholas Bence <nicholasbence12@gmail.com>
2026-08-28 20:13:15 +0200

    Remove default MVC scaffold not tied to assignment deliverables
    Deletes HomeController, ErrorViewModel, Home/Error views and the
    Home/Error exception handler wiring in Program.cs -- none of these
    correspond to a scenario deliverable.

--- ITEHA3-B33 - Formative - Tygervalley - EDUV4846233/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace ITEHA3_B33___Formative___Tygervalley___EDUV4846233.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
